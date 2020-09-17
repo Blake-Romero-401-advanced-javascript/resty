8b0be368c35f25eecf75dfa208f15dc14e99e6fe
blakerom <blakeromero001@gmail.com>
2020-09-16 23:39:49 -0700

    finishing up day 2 work having issues with react

--- app/src/App.js
+++ app/src/App.js
@@ -1,65 +1,33 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Form from './components/Form';
+import Results from './components/Results';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Form />
-      {/* <Results /> */}
-    </div>
-  )
-}
-
-class Form extends React.Component {
+class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      words:'Enter URL',
+      count: 0,
+      headers: {},
+      results: [],
     }
   }
 
-  handleWord = event => {
-    let words = event.target.value;
-    this.setState({words});
+  handleForm = (count, results, headers) => {
+    this.setState({ count, results, headers });
   }
-
-  handleClick = event => {
-    event.preventDefault();
-    let words = this.state.words
-      .split('')
-      .reverse()
-      .join('');
-
-    this.setState({ words });
-    
-  }
-
+  
   render() {
     return (
-      <form>
-        <h3>{this.state.words}</h3>
-        <input type="text" onChange={this.handleWord} />
-        <option type="radio">Get</option>
-        <div role="group">
-          {/* <label>GET</label> */}
-          <input type="button" value="GET" checked="checked" onClick={this.handleClick} />
-          {/* <label>DELETE</label> */}
-          <input type="button" value="DELETE" onClick={this.handleClick} />
-          {/* <label>PUT</label> */}
-          <input type="button" value="PUT" onClick={this.handleClick} />
-          {/* <label>POST</label> */}
-          <input type="button" value="POST" onClick={this.handleClick} />
-        </div>
-        {/* <button onClick={this.handleClick}>GET</button>
-        <button onClick={this.handleClick}>DELETE</button>
-        <button onClick={this.handleClick}>PUT</button>
-        <button onClick={this.handleClick}>POST</button> */}
-
-
-      </form>
+      <div className='App'>
+        <Header />
+        <Form handler={this.handleForm}/>
+        <Results count={this.state.count} results={this.state.results} headers={this.state.headers}/>
+        <Footer />
+      </div>
     );
   }
 }
@@ -67,71 +35,3 @@ class Form extends React.Component {
 
 
 export default App;
-
-
-
-
-
-
-
-// import React from 'react';
-
-// import './app.scss';
-
-// const Header = () => {
-//   return (
-//     <header>
-//       <h1>New header</h1>
-//     </header>
-//   );
-// };
-
-// const Footer = () => <footer>&copy; 2020 Blake Romero</footer>;
-
-// class Main extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     let params = new URL(document.location).searchParams;
-//     this.state = {
-//       words: params.words || 'nothing to see here',
-//     };
-//   }
-
-//   handleWord = e => {
-//     let words = e.target.value;
-//     this.setState({ words });
-//   };
-
-//   handleClick = e => {
-//     e.preventDefault();
-//     let words = this.state.words
-//       .split('')
-//       .reverse()
-//       .join('');
-//     this.setState({ words });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h3>{this.state.words}</h3>
-//         <input onChange={this.handleWord} />
-//         <button onClick={this.handleClick}>Click Me</button>
-//       </div>
-//     );
-//   }
-// }
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <Header />
-//         <Main />
-//         <Footer />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-// export default App;
---
+++ app/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>&copy; 2020 Blake</footer>
+  );
+}
+
+export default Footer;
